defbfb22f3985cb2b932989b61839af0b3d1667d
gksolutions.work007 <gksolutions.work007@gmail.com>
2026-01-29 21:00:01 +0530

    web coll changes

---
+++ ShineWeb/Controllers/MobileCustomerOSController.cs
@@ -0,0 +1,36 @@
+﻿using ShineWeb.BuisnessLayer;
+using ShineWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShineWeb.Controllers
+{
+    public class MobileCustomerOSController : Controller
+    {
+        // GET: MobileCustomerOS
+        public ActionResult Index(string Name, string strFormID)
+        {
+            if (Session["LoginUserID"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                Name = clsEncryptDecrypt.Decrypt(Name);
+                string decFormID = clsEncryptDecrypt.Decrypt(strFormID);
+                ViewData["FormName"] = Name;
+                ViewData["FormID"] = decFormID;
+                DataTable dtPermission = (System.Data.DataTable)Session["dtPermission"];
+                string AddPerm = dtPermission.Select("MenuID = 248", null).Length > 0 ? "1" : "0";
+                SingleMasterModel dam = new SingleMasterModel();
+                dam.FormName = Name;
+                dam.Add = AddPerm;
+                return View(dam);
+            }
+        }    
+    }
+}
