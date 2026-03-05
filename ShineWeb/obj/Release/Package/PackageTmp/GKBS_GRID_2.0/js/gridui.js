15b00cab9e8729779849f7a1dde8ec2b84d90182
gksolutions.work007 <gksolutions.work007@gmail.com>
2026-03-05 10:24:05 +0530

    init push

--- ShineWeb/obj/Release/Package/PackageTmp/GKBS_GRID_2.0/js/gridui.js
+++ ShineWeb/obj/Release/Package/PackageTmp/GKBS_GRID_2.0/js/gridui.js
@@ -651,7 +651,7 @@ class GKBSDynamicGrid {
             }
 
             // Only calculate for 'number' types
-            if (col.type === 'number' || col.Total === true) {
+            if (col.type === 'number' || col.Total === true || col.EnableCount || col.EnableUnique) {
                 const values = dataToCalculate
                     .map(row => parseFloat(row[col.field]))
                     .filter(val => !isNaN(val)); // Filter out bad data
@@ -661,14 +661,29 @@ class GKBSDynamicGrid {
                     const sum = values.reduce((a, b) => a + b, 0);
                     // 2. Calculate Average
                     const avg = sum / values.length;
-
+                    // 3. Unique Records
+                    var UniqueCounts = 0;
+                    if (col.EnableUnique) {
+                        UniqueCounts = new Set(dataToCalculate.map(item => item[col.field])).size;
+                        console.log("have no : " + col.field, Object.values(values));
+                    }
                     // Format numbers (e.g., 12,300.50)
                     const fmt = (n) => n.toLocaleString(undefined, { minimumFractionDigits: 0, maximumFractionDigits: 2 });
 
                     cell.innerHTML = `                    
+                    `+ (col.EnableCount ? ` <div><span class="dg-stat-label">Count:</span>${dataToCalculate.length}</div> ` : ``) + ` 
+                    `+ (col.EnableUnique ? ` <div><span class="dg-stat-label">Unique:</span>${UniqueCounts}</div> ` : ``) + ` 
                         `+ (col.EnableSum ? ` <div><span class="dg-stat-label">Sum:</span>${fmt(sum)}</div> ` : ``) + `
                         `+ (col.EnableAvg ? ` <div><span class="dg-stat-label">Avg:</span>${fmt(avg)}</div> ` : ``) + `
                     `;
+                }
+                else {
+                    // 
+                    const uniqueCount = new Set(dataToCalculate.map(item => item[col.field])).size;
+                    console.log(col.field, dataToCalculate.map(item => item[col.field]));
+                    cell.innerHTML = `                    
+                    `+ (col.EnableCount ? ` <div><span class="dg-stat-label">Count:</span>${dataToCalculate.length}</div> ` : ``) + ` 
+                    `+ (col.EnableUnique ? ` <div><span class="dg-stat-label">Unique:</span>${uniqueCount}</div> ` : ``) + ``;
                 }
             } else {
                 // Optional: Show Count for text columns
