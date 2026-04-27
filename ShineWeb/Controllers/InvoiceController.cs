dfabacc2effebb5f5c597c518ef2ca0cc33f1d23
GKBSDEV <gksolutions.work007@gmail.com>
2026-04-27 20:04:06 +0530

    new Autolist Implement

--- ShineWeb/Controllers/InvoiceController.cs
+++ ShineWeb/Controllers/InvoiceController.cs
@@ -40,7 +40,7 @@ namespace ShineWeb.Controllers
                 string ViewPerm = dtPermission.Select("MenuName = 'View " + Name + "'", null).Length > 0 || nUID == 1 ? "1" : "0";
                 string CanPerm = dtPermission.Select("MenuName = 'Cancel " + Name + "'", null).Length > 0 || nUID == 1 ? "1" : "0";
                 string VarPerm = dtPermission.Select("MenuName = 'Variant " + Name + "'", null).Length > 0 || nUID == 1 ? "1" : "0";
-                string EnbBranch = dtPermission.Select("MenuID = 221", null).Length > 0 || nUID == 1 ? "1" : "0";
+                string EnbBranch = nUID == 1 ? "0" : dtPermission.Select("MenuID = 221", null).Length > 0 ? "1" : "0";
                 string EnabSave = dtPermission.Select("MenuID = 256", null).Length > 0 || nUID == 1 ? "1" : "0";
                 string editprice = dtPermission.Select("MenuID = 321", null).Length > 0 || nUID == 1 ? "1" : "0";
                 string DrftMod = dtPermission.Select("MenuID = 388", null).Length > 0 || nUID == 1 ? "1" : "0";
