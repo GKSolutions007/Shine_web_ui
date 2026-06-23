c5be62b9445ef5f7d91743e7ba6191f1626c7d0a
gksolutions.work007 <gksolutions.work007@gmail.com>
2026-06-23 19:27:19 +0530

    Bulk collection, invoice edit doc load speed

--- ShineWeb/obj/Release/Package/PackageTmp/Scripts/Validation.js
+++ ShineWeb/obj/Release/Package/PackageTmp/Scripts/Validation.js
@@ -704,6 +704,21 @@ function AssignDates(FromFieldID, ToFieldID, Type) {
         });
     }
 }
+function getCurrentDate() {
+    var today = new Date();
+    var dd = today.getDate();
+    var mm = today.getMonth() + 1; //January is 0!
+    var yyyy = today.getFullYear();
+    if (dd < 10) {
+        dd = '0' + dd;
+    }
+
+    if (mm < 10) {
+        mm = '0' + mm;
+    }
+    today = yyyy + '-' + mm + '-' + dd;
+    return today;
+}
 document.addEventListener("keydown", function (event) {
     if (event.key === "F5") {        
         event.preventDefault();
