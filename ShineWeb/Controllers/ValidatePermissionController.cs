a5820c91c6dbc9e23d209aba565598cace933453
gksolutions.work007 <gksolutions.work007@gmail.com>
2026-08-27 11:16:18 +0530

    ui cchanges

--- ShineWeb/Controllers/ValidatePermissionController.cs
+++ ShineWeb/Controllers/ValidatePermissionController.cs
@@ -100,12 +100,13 @@ namespace ShineWeb.Controllers
                 Session["DefaultCustID"] = dtAppconfig.Rows[0]["DefaultCustID"].ToString();
                 Session["SelectinvoiceinSR"] = dtAppconfig.Rows[0]["SelectinvoiceinSR"].ToString();
                 Session["InvoiceStockOnlyProduct"] = dtAppconfig.Rows[0]["InvoiceStockOnlyProduct"].ToString();
+                Session["AssignInvBeatSalesmanfrom"] = dtAppconfig.Rows[0]["AssignInvBeatSalesmanfrom"].ToString();
+                
                 Session["FilterDateType"] = dtAppconfig.Rows[0]["FilterDate"].ToString();
                 Session["PurchaseOneView"] = dtAppconfig.Rows[0]["PurchaseOneView"].ToString();
                 Session["SalesOneView"] = dtAppconfig.Rows[0]["SalesOneView"].ToString();
                 Session["AllSalesmanInvoice"] = dtAppconfig.Rows[0]["AllSalesmanInvoice"].ToString();
                 Session["SelectinvoiceinSR"] = dtAppconfig.Rows[0]["SelectinvoiceinSR"].ToString();
-                Session["InvoiceStockOnlyProduct"] = dtAppconfig.Rows[0]["InvoiceStockOnlyProduct"].ToString();
                 Session["FilterDateType"] = dtAppconfig.Rows[0]["FilterDate"].ToString();
                 Session["PurchaseOneView"] = dtAppconfig.Rows[0]["PurchaseOneView"].ToString();
                 Session["SalesOneView"] = dtAppconfig.Rows[0]["SalesOneView"].ToString();
