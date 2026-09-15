67c52ef308cc5a21731a3a89fa9fceb82befed29
GKBSDEV <gkbsdev@gmail.com>
2026-09-15 19:59:22 +0530

    Update Financial Date added to tools

---
+++ ShineWeb/Controllers/UpdateFinancialDateController.cs
@@ -0,0 +1,41 @@
+﻿using ShineWeb.BuisnessLayer;
+using ShineWeb.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data;
+using System.Web.Mvc;
+
+namespace ShineWeb.Controllers
+{
+    public class UpdateFinancialDateController : Controller
+    {
+        // GET: UpdateFinancialDate
+        public ActionResult Index(string Name, string strFormID)
+        {
+            if (Session["LoginUserID"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                int UID = Convert.ToInt32(Session["LoginUserID"]);
+                DataTable dtPermission = (System.Data.DataTable)Session["dtPermission"];
+                bool Formhaveanypermission = dtPermission.Select("MenuId = 566", null).Length > 0;
+                if (!Formhaveanypermission && UID != 1)
+                {
+                    return RedirectToAction("nopermission", "Home",
+                        new { emanmrofnoissimerpon = "qprXzPvk/rvwtzC4H3cBvcMJY1Wi8QhYs90gPRyprCxj2UeyJB9l8OO8reGZiiC/" });
+                }
+                Name = clsEncryptDecrypt.Decrypt(Name);
+                string decFormID = clsEncryptDecrypt.Decrypt(strFormID);
+                ViewData["FormName"] = Name;
+                ViewData["FormID"] = decFormID;
+                SingleMasterModel dam = new SingleMasterModel();
+                dam.FormName = Name;
+                return View(dam);
+            }
+        }
+    }
+}
