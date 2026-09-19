f1d2e1346ef44d44744fd3d1dd09b669a6193add
GKBSDEV <gkbsdev@gmail.com>
2026-09-19 20:33:48 +0530

    Customer Location Verification

---
+++ ShineWeb/Controllers/CustomerLocationVerificationController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ShineWeb.Controllers
+{
+    public class CustomerLocationVerificationController : Controller
+    {
+        // GET: CustomerLocationVerification
+        public ActionResult Index()
+        {
+            if (Session["LoginUserID"] == null)
+            {
+                return RedirectToAction("Index", "Login");
+            }
+            else
+            {
+                int UID = Convert.ToInt32(Session["LoginUserID"]);
+                DataTable dtPermission = (System.Data.DataTable)Session["dtPermission"];
+                bool Formhaveanypermission = dtPermission.Select("MenuId = 543", null).Length > 0;
+                if (!Formhaveanypermission && UID != 1)
+                {
+                    return RedirectToAction("nopermission", "Home",
+                        new { emanmrofnoissimerpon = "tb+f9O2Wm3tg5NtQhzujtANya9bd+M01W71UAM02/zoOpChCv+3XaD9iSVRRWnK6" });
+                }
+                return View();
+            }
+        }
+    }
+}
